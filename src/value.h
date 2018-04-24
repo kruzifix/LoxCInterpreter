4b7fc123ede6d10150685d4d66a35218b588e4e0
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-24 23:00:41 +0200

    added value array

---
+++ src/value.h
@@ -0,0 +1,18 @@
+#ifndef clox_value_h
+#define clox_value_h
+
+#include "common.h"
+
+typedef double value_t;
+
+typedef struct {
+    int count;
+    int capacity;
+    value_t* values;
+} value_array_t;
+
+void init_value_array(value_array_t* array);
+void write_value_array(value_array_t* array, value_t value);
+void free_value_array(value_array_t* array);
+
+#endif
