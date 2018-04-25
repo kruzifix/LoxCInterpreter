952ebb5e7b98a41acf85a3f9694b054531c886f0
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-25 17:34:45 +0200

    balbla

--- src/main.c
+++ src/main.c
@@ -6,11 +6,8 @@ int main(int argc, const char* argv[])
 {
     chunk_t chunk;
     init_chunk(&chunk);
-    
-    for (int i = 0; i < 400; i++)
-    {
-        write_constant(&chunk, i * 0.1, i);
-    }
+
+    write_constant(&chunk, 12.4, 123);
 
     write_chunk(&chunk, OP_RETURN, 123);
 
