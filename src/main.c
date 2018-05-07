f7c065f64e7e590b44763a7cf93ce576994890ce
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-05-07 23:09:25 +0200

    scanning!

--- src/main.c
+++ src/main.c
@@ -1,74 +1,171 @@
-//#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
-//#include "trie.h"
+#include "trie.h"
 
-/*
-#define OK 1
-#define ERROR 0
+typedef enum {
+    TOK_EOF,
+    TOK_OP,
+    TOK_NUM,
+    TOK_ID,
+    TOK_FALSE,
+    TOK_TRUE
+} token_type_t;
 
-int lookahead;
+typedef struct {
+    token_type_t type;
+    union {
+        int value;
+        char* lexeme;
+    };
+} token_t;
 
-int isDigit(int i)
+char* read_file(const char* filename)
 {
-    if (i >= '0' && i <= '9')
-        return OK;
-    return ERROR;
-}
-
-int match(int t)
-{
-    if (t == lookahead)
+    FILE* f = fopen(filename, "r");
+    if (!f)
+        return NULL;
+    fseek(f, 0, SEEK_END);
+    long length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    char* buffer = malloc(length + 1);
+    if (buffer)
     {
-        lookahead = getchar();
-        return OK;
+        memset(buffer, '\0', length + 1);
+        fread(buffer, 1, length, f);
     }
-    return ERROR;
+    fclose(f);
+    return buffer;
 }
 
-int term()
-{
-    if (isDigit(lookahead))
-    {
-        printf("%c", lookahead);
-        return match(lookahead);
-    }
-    return ERROR;
-}
+#define IS_DIGIT(c) ((c) >= '0' && (c) <= '9')
+#define IS_LETTER(c) (((c) >= 'a' && (c) <= 'z') || ((c) >= 'A' && (c) >= 'Z'))
 
-int expr()
+int line = 1;
+char* peek = NULL;
+trie_node_t* words = NULL;
+
+void scan(token_t* tok)
 {
-    if (term() == ERROR)
-        return ERROR;
-    while (true)
+    for (; *peek; ++peek)
     {
-        if (lookahead == '+')
-        {
-            match('+');
-            if (term() == ERROR)
-                return ERROR;
-            printf("+");
-        }
-        else if (lookahead == '-')
-        {
-            match('-');
-            if (term() == ERROR)
-                return ERROR;
-            printf("-");
-        }
+        if (*peek == ' ' || *peek == '\t' || *peek == '\r')
+            continue;
+        else if (*peek == '\n')
+            line++;
         else
-            return OK;
+            break;
     }
+
+    if (!*peek)
+    {
+        tok->type = TOK_EOF;
+        return;
+    }
+
+    if (IS_DIGIT(*peek))
+    {
+        int v = 0;
+        do {
+            v = 10 * v + (*peek - '0');
+            ++peek;
+        } while (IS_DIGIT(*peek));
+
+        tok->type = TOK_NUM;
+        tok->value = v;
+        return;
+    }
+    else if (IS_LETTER(*peek))
+    {
+        char* start = peek;
+        do {
+            ++peek;
+        } while (IS_LETTER(*peek) || IS_DIGIT(*peek));
+
+        size_t len = (peek - start);
+        char* word = malloc(len + 1);
+        memset(word, '\0', len + 1);
+        strncpy(word, start, len);
+
+        int type = trie_contains(words, word);
+
+        if (type >= 0)
+        {
+            free(word);
+            tok->type = type;
+            return;
+        }
+
+        trie_insert(&words, word, TOK_ID);
+
+        tok->type = TOK_ID;
+        tok->lexeme = word;
+        return;
+    }
+
+    tok->type = TOK_OP;
+    tok->value = *peek++;
 }
-*/
 
 int main(int argc, const char* argv[])
 {
+    if (argc != 2)
+    {
+        printf("expected file path\n");
+    }
+
+    char* content = read_file(argv[1]);
+    if (!content)
+    {
+        printf("empty file\n");
+        return 1;
+    }
+    peek = content;
+    trie_insert(&words, "true", TOK_TRUE);
+    trie_insert(&words, "false", TOK_FALSE);
+
+    token_t tok;
+    do {
+        scan(&tok);
+        switch (tok.type)
+        {
+        case TOK_ID:
+            printf("<ID, %s>\n", tok.lexeme);
+            free(tok.lexeme);
+            break;
+        case TOK_NUM:
+            printf("<NUM, %i>\n", tok.value);
+            break;
+        case TOK_EOF:
+            printf("<EOF>\n");
+            break;
+        case TOK_OP:
+            printf("<OP, %c>\n", tok.value);
+            break;
+        case TOK_FALSE:
+            printf("<false>\n");
+            break;
+        case TOK_TRUE:
+            printf("<true>\n");
+            break;
+        default:
+            printf("<%c>\n", tok.value);
+            break;
+        }
+    } while (tok.type != TOK_EOF);
+
+    free(content);
+    trie_free(&words);
+
+    return 0;
+
     /*
     trie_node_t* root = NULL;
 
@@ -91,27 +188,6 @@ int main(int argc, const char* argv[])
     return 0;
     */
 
-    /*
-    lookahead = getchar();
-    int res = expr();
-    if (res == ERROR)
-        printf("\nsyntax error!");
-    return;
-    */
-    /*if (argc == 2)
-    {
-        FILE* file = fopen(argv[1], "r");
-        if (file)
-        {
-            int c;
-            while ((c = getc(file)) != EOF)
-            {
-                putchar(c);
-            }
-            fclose(file);
-        }
-    }*/
-    
     init_vm();
 
     chunk_t chunk;
