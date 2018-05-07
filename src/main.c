1372ce74774703e623583bfb0589d5eb97553725
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-05-07 18:11:33 +0200

    added infix to postfix parser

--- src/main.c
+++ src/main.c
@@ -6,10 +6,74 @@
 #include "debug.h"
 #include "vm.h"
 
+/*
+#define OK 1
+#define ERROR 0
 
+int lookahead;
+
+int isDigit(int i)
+{
+    if (i >= '0' && i <= '9')
+        return OK;
+    return ERROR;
+}
+
+int match(int t)
+{
+    if (t == lookahead)
+    {
+        lookahead = getchar();
+        return OK;
+    }
+    return ERROR;
+}
+
+int term()
+{
+    if (isDigit(lookahead))
+    {
+        printf("%c", lookahead);
+        return match(lookahead);
+    }
+    return ERROR;
+}
+
+int expr()
+{
+    if (term() == ERROR)
+        return ERROR;
+    while (true)
+    {
+        if (lookahead == '+')
+        {
+            match('+');
+            if (term() == ERROR)
+                return ERROR;
+            printf("+");
+        }
+        else if (lookahead == '-')
+        {
+            match('-');
+            if (term() == ERROR)
+                return ERROR;
+            printf("-");
+        }
+        else
+            return OK;
+    }
+}
+*/
 
 int main(int argc, const char* argv[])
 {
+    /*
+    lookahead = getchar();
+    int res = expr();
+    if (res == ERROR)
+        printf("\nsyntax error!");
+    return;
+    */
     /*if (argc == 2)
     {
         FILE* file = fopen(argv[1], "r");
@@ -23,7 +87,7 @@ int main(int argc, const char* argv[])
             fclose(file);
         }
     }*/
-
+    
     init_vm();
 
     chunk_t chunk;
