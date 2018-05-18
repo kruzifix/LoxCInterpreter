7b383a1c57ed84be86efd23d8fd15f55b6329c74
David Cukrowicz <david.cukro@gmail.com>
2018-05-18 19:40:56 +0200

    added scanner start

--- src/scanner.c
+++ src/scanner.c
@@ -18,3 +18,37 @@ void init_scanner(const char* source)
     scanner.current = source;
     scanner.line = 1;
 }
+
+static bool is_at_end()
+{
+    return !*scanner.current;
+}
+
+static token_t make_token(token_type_t type)
+{
+    token_t token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+    return token;
+}
+
+static token_t error_token(const char* message)
+{
+    token_t token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int)strlen(message);
+    token.line = scanner.line;
+    return token;
+}
+
+token_t scan_token()
+{
+    scanner.start = scanner.current;
+
+    if (is_at_end()) return make_token(TOKEN_EOF);
+
+    return error_token("Unexpected character.");
+}
